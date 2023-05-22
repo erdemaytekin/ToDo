0f6ee6ad5630dabe03050e614e52d58154f0e6db
erdemaytekin <116784884+erdemaytekin@users.noreply.github.com>
2023-05-23 01:46:47 +0300

    Update UserSessionManager.java

--- app/src/main/java/com/example/noteapp/UserSessionManager.java
+++ app/src/main/java/com/example/noteapp/UserSessionManager.java
@@ -1,5 +1,21 @@
 package com.example.noteapp;
 
+
+
+
+
+//Bu kod parçacığı, bir Android uygulaması için bir UserSessionManager sınıfı tanımlar.
+   // Bu sınıf, kullanıcının oturum bilgilerini yönetmek için kullanılır. Sınıfın içinde SharedPreferences ve SharedPreferences.
+  //  Editor nesneleri bulunur. Bu nesneler, uygulamanın paylaşılan tercihlerini okuma ve yazma işlemlerini gerçekleştirmek için kullanılır.
+
+//UserSessionManager sınıfının içinde üç adet metod bulunur: saveUserId, getUserId ve logout. 
+   // saveUserId metodu, kullanıcının ID’sini paylaşılan tercihlere kaydeder. getUserId metodu, 
+//paylaşılan tercihlerden kullanıcının ID’sini alır ve döndürür. Eğer kullanıcı ID’si kaydedilmemişse, bu metod -1 değerini döndürür. 
+  //  logout metodu ise paylaşılan tercihlerden kullanıcı ID’sini kaldırır.
+
+
+
+
 import android.content.Context;
 import android.content.SharedPreferences;
 
